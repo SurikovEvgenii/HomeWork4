6b520de33062db061e35a5eab102a72793d29ddf
Surikov Evgenii <surikov96@mail.ru>
2024-02-28 19:51:35 +0700

    Add check string select on null. Add check String[] numString on null

--- src/main/java/com/surikov/homework4web/ValueServlet.java
+++ src/main/java/com/surikov/homework4web/ValueServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @WebServlet(name="calculateValueServlet", value = "/calculatevalue")
 public class ValueServlet extends HttpServlet {
@@ -19,45 +20,42 @@ public class ValueServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        HttpSession session = req.getSession();
         String[] numsString = req.getParameterValues("num");
         String select = req.getParameter("radioSelect");
-        int[] numsInt = new int[numsString.length];
-        int calculateValue;
+        int[] numsInt = numsInt = new int[numsString.length];;
+        Service service = new Service();
+        int calculateValue = 0;
 
-        for(int i=0;i<numsString.length;i++){
-            numsInt[i] = Integer.parseInt(numsString[i]);
-            session.setAttribute("num"+i,numsInt[i]);
-        }
+                if(select == null) {
+                    select = "Result";
+                }
 
-        calculateValue = numsInt[0];
-
-        switch(select){
-            case "MAX":
-                for(int i=0;i<numsInt.length;i++){
-                    if(calculateValue<numsInt[i]){
-                        calculateValue = numsInt[i];
+                for(int i=0;i<numsString.length;i++){
+                    if(numsString[i].isEmpty()){
+                        numsInt[i] = 0;
+                    } else {
+                        numsInt[i] = Integer.parseInt(numsString[i]);
                     }
-                }
-                break;
-            case "MIN":
-                for(int i=0;i<numsInt.length;i++){
-                    if(calculateValue>numsInt[i]){
-                        calculateValue = numsInt[i];
-                    }
-                }
-                break;
-            case "AVG":
-                int sum=0;
-                for(int i=0;i<numsInt.length;i++){
-                    sum +=numsInt[i];
-                }
-                calculateValue = sum/numsInt.length;
-        }
 
-        session.setAttribute("calculateValue",calculateValue);
-        session.setAttribute("checkBoxValue",select);
-        session.getServletContext().getRequestDispatcher("/calculatevalue.jsp").forward(req,resp);
+                    req.setAttribute("num"+i,numsInt[i]);
+                }
+
+                switch(select){
+                    case "MAX":
+                        calculateValue = service.getMax(numsInt);
+                        break;
+                    case "MIN":
+                        calculateValue = service.getMin(numsInt);
+                        break;
+                    case "AVG":
+                        calculateValue = service.getAvg(numsInt);
+                        break;
+                }
+
+                req.setAttribute("calculateValue",calculateValue);
+                req.setAttribute("checkBoxValue",select);
+                req.getServletContext().getRequestDispatcher("/calculatevalue.jsp").forward(req,resp);
+
 
 
     }
